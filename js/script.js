ab494e758c96c42a81d67d959207eada4f6d50e5
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-03-14 12:16:49 +0100

    feat: :sparkles: Adds demo for today's exercise

--- js/script.js
+++ js/script.js
@@ -111,6 +111,7 @@ for (let i = 0; i < items.length; i++) {
 } 
 */
 
+/* 
 // PARTE 3 : METODI DIVERSI DI ARRAY
 // Ne vediamo alcuni
 
@@ -125,3 +126,63 @@ console.log(studentsString);
 const hasStudent = students.includes("Diego");
 console.log(hasStudent);
  // controllo, senza usare alcun ciclo "for"
+
+ // Ordinare dentro un array:
+ // in questo caso, in maniera alfabetica
+ // Sort
+
+ const myNumbers = [34, 1, 100, 65];
+ console.log(myNumbers.sort);
+
+// Sort non funziona con i numeri, ma con le lettere...
+ */
+
+////////////////////////////////////////////////////////////////////////
+
+// DEMO ESERCIZIO "SLIDER"
+
+// Creare slide dinamicamente
+const images = ["", "", "", "", "",];
+
+for (let i = 0; i < imagesArray.lenght; i++) {
+    const currentImage = imagesArray[i];
+    
+    const sliderItem = `
+    <div class="item">
+                    <img src="${currentImage}" alt="">
+                </div>
+    `;
+
+    itemsContainer.innerHTML += sliderItem;
+}
+
+// Stato di partenza: 
+let itemsArray = document.getElementsByClassName("item");
+console.log(itemsArray);
+
+// imposto il primo slide visibile
+let activeItemIndex = 0;
+itemsArray[activeItemIndex].classList.add("active");
+
+
+// Gestisco il click sul bottone next
+const nextBtn = document.querySelector(".next");
+nextBtn.addEventListener("click", function() {
+    if (activeItemIndex < (itemsArray.lenght - 1)) {
+
+        // rimuovere active dallo slide corrente
+        itemsArray[activeItemIndex].classList.remove("active");
+
+        //incremento activeIndex
+        activeItemIndex++;
+
+        // aggiungo active a quello nuovo
+        itemsArray[activeItemIndex].classList.add("active");
+
+        // Se siamo arrivati all'ultimo slide
+        // Nascondo il bottone
+        if (activeItemIndex === itemsArray.lenght - 1) {
+            nextBtn.classList.add("hidden");
+        }
+    }
+});
